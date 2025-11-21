520ef638098d94038d98ab96a45cef6b339d8f83
mailpeters <dpiddy@protonmail.com>
2025-11-21 15:34:38 -0500

    map

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
 import AuthSessionProvider from "@/components/session-provider";
 
 const geistSans = Geist({
